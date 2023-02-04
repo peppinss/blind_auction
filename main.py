6ba3c50c3545deb61765e5496610e6bf1c13250c
peppinss <peppeke123@gmail.com>
2023-02-04 03:09:56 +0000

    v1.1

--- main.py
+++ main.py
@@ -1,24 +1,81 @@
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62,
-}
-
-
-student_grades = {}
-for x in student_scores:
-    uga = int(student_scores[x])
-    if uga >= 91:
-        student_grades[x] = "Outstanding"
-    elif uga >= 81:
-        student_grades[x] = "Exceeds Expectations"
-    elif uga >= 71:
-        student_grades[x] = "Acceptable"
-    else:
-        student_grades[x] = "Fail"
+from logo import logo
 
 
 
-print(student_grades)
+
+
+def playagain():
+    while True:
+        variabile1 = input("Vuoi giocare ancora? Y N")
+        if variabile1 == "y" or variabile1 == "yes":
+            return True
+        elif variabile1 == "n" or variabile1 == "no":
+            return False
+        else:
+            print("Il valore inserito non sembra correto, prova di nuovo")
+
+
+
+def checkwinner(database):
+    check2 = 0
+    for x in database:
+        check1 = database[x]
+        if check1 > check2:
+            score1 = x
+    print(f"Congratulazioni {score1} ha vinto puntando {check1}")
+
+
+def bid():
+    while True:
+        try:
+            bid1 = int(input("inserisci la tua puntata\n"))
+            return bid1
+        except:
+            print("il valore inserito non sembra correto, riprova")
+
+
+
+def name(database, firstrun):
+    while True:
+        name = input("Inserisci il tuo nome:\n").lower()
+        if firstrun == 1:
+            for x in database:
+                if name in x:
+                    print("Il nome inserito e' gia' stato preso")
+                else:
+                    return name
+        else:
+            return name
+
+
+
+def main():
+    database = {}
+    check = True
+    firstrun = 0
+    while check:
+        name1 = name(database, firstrun)
+        if firstrun == 0:
+            firstrun = 1
+        bid1 = bid()
+        database[name1] = bid1
+        while True:
+            again = input("Ci sono altre persone che voglio puntare Y N").lower()
+            if again == "y":
+                break
+            elif again == "n":
+                check = False
+                break
+            else:
+                print("Il valore inserito non sembra corretto riprova")
+    checkwinner(database)
+
+
+
+
+if __name__ == "__main__":
+    costanzo = True
+    while costanzo:
+        print(logo)
+        main()
+        costanzo =playagain()
